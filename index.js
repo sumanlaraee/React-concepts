3e7a12b0ce431a45b2787092726291c6fa81a6d1
suman bai <127082344+sumanlaraee@users.noreply.github.com>
2023-08-19 12:42:58 -0700

    Add files via upload
    basically this was a simple task using react.js 
    Task 1 : 
    >>create react app from scratch 
    >>add one h1 element in 
    >>add one p element 
    >>add list of fav 5 netflix series 
    using an ordered list

--- index.js
+++ index.js
@@ -6,16 +6,4 @@ import ReactDom  from "react-dom";
 //note : only remove multiline comment of piece of code to see how piece of code works 
 //single line comments are comments for understandilbilty
 
-// in previous codes  we have seen we were using <div> to render multiple elements 
-// when we use grid/ flexbox this extra div create problems so we use react fragment instead of div
-
-//reat fragment <React.fragment><React.fragment/> or if version is 16+ use only <></>
-
-
-ReactDom.render(
-  <>
-    <h1>hello miss suman laraee</h1>
-    <p>i hope you are doing good </p>
-  </>, document.getElementById('root')
-)
 
