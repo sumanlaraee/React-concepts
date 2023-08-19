90d8d7e14f121b9c703991774a05fa5c3f7a2fd0
suman bai <127082344+sumanlaraee@users.noreply.github.com>
2023-08-19 09:15:48 -0700

    Add files via upload
    here iam writing a simple program of hello world in react using Dom of html by accessing id from index.html

---
+++ index.js
@@ -0,0 +1,12 @@
+//old js code 
+/*
+var React = require('react');
+var ReactDom= require('react-dom') //using es5
+
+*/
+
+// modern js sode babel support this and babel come from npm pkg while installing 
+import React from "react";
+import ReactDom  from "react-dom";
+
+ReactDom.render(<h1>hello suman</h1>, document.getElementById('root'))
