5e6bb557c0ff17f70a91763c20d689b77a4cb9d9
suman bai <127082344+sumanlaraee@users.noreply.github.com>
2023-08-19 14:43:09 -0700

    Add files via upload
    in this code iam using js expression and es6 template literals

--- index.js
+++ index.js
@@ -7,3 +7,41 @@ import ReactDom  from "react-dom";
 //single line comments are comments for understandilbilty
 
 
+/*
+//we had rendered single element then multiple elements used jsx , javascript , pure react , react fragment
+
+// now we are going to use javascript Expression  basically we use curly braces for expressions and variables in jsx
+// you cannt use if else or condition inside {} in jsx 
+
+const greeting ="Radhey Radhey"
+ReactDom.render(
+<>
+<h1>{greeting}</h1>
+
+<p>thi year is {2000+23}</p>
+
+<p>{Math.random()}</p>            
+
+</>
+, document.getElementById('root'))
+
+*/
+
+const fname ="suman"
+const lname ="laraee"
+/*
+
+// we are gonna use ES6 template litral 
+//method 1 :
+ReactDom.render(<h1>my name is {fname} {lname}</h1>, document.getElementById('root'))
+*/
+
+/*
+// we are gonna use ES6 template litral 
+//method 2 :
+ReactDom.render(<h1>my name is {fname +" "+lname}</h1>, document.getElementById('root'))
+*/
+
+// we are gonna use ES6 template litral 
+//method 3 :
+ReactDom.render(<h1>{`hi my name is ${fname } ${lname}`}</h1>, document.getElementById('root'))
