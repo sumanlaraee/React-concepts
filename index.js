6be0ee34d92f51c16f52ae7bd2a0e6d3ba30fe32
suman bai <127082344+sumanlaraee@users.noreply.github.com>
2023-08-21 08:12:39 -0700

    Add files via upload
    using image along with styling gonna upload css code also.

--- index.js
+++ index.js
@@ -1,47 +1,25 @@
-
-import React from "react";        // if we wanna use jsx we need to import React module 
-import ReactDom  from "react-dom";
-
-
-//note : only remove multiline comment of piece of code to see how piece of code works 
-//single line comments are comments for understandilbilty
-
-
-/*
-//we had rendered single element then multiple elements used jsx , javascript , pure react , react fragment
-
-// now we are going to use javascript Expression  basically we use curly braces for expressions and variables in jsx
-// you cannt use if else or condition inside {} in jsx 
-
-const greeting ="Radhey Radhey"
+import React from 'react';
+import ReactDom from "react-dom";
+import "./index.css";
+// using css in react , importing css file in react  we will move to index.css 
+const img1="https://picsum.photos/200/300";
+const img2="https://picsum.photos/250/300"
+const img3="https://picsum.photos/300/300"
 ReactDom.render(
 <>
-<h1>{greeting}</h1>
-
-<p>thi year is {2000+23}</p>
-
-<p>{Math.random()}</p>            
+<h1 className="heading ">hello my name is suman</h1>
+<div className="img_div">
+<img src={img1} alt="random img "></img>
+<img src={img2} alt="random img "></img>
+<img src={img3} alt="random img "></img>
+</div>
 
 </>
-, document.getElementById('root'))
+, document.getElementById("root"))
 
-*/
 
-const fname ="suman"
-const lname ="laraee"
-/*
 
-// we are gonna use ES6 template litral 
-//method 1 :
-ReactDom.render(<h1>my name is {fname} {lname}</h1>, document.getElementById('root'))
-*/
 
-/*
-// we are gonna use ES6 template litral 
-//method 2 :
-ReactDom.render(<h1>my name is {fname +" "+lname}</h1>, document.getElementById('root'))
-*/
 
-// we are gonna use ES6 template litral 
-//method 3 :
-ReactDom.render(<h1>{`hi my name is ${fname } ${lname}`}</h1>, document.getElementById('root'))
+
+
