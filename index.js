eec51bc6cbdaa97c5b05e56c6f4fec9c65727997
suman bai <127082344+sumanlaraee@users.noreply.github.com>
2023-08-21 09:46:39 -0700

    Add files via upload
    in this case we are learning about internal and inline css with jsx in react(using javascript object for inline css)

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ import "./index.css";
 const img1="https://picsum.photos/200/300";
 const img2="https://picsum.photos/250/300"
 const img3="https://picsum.photos/300/300"
+
+// wehn we are using inline css we need to remmember js objects  
+//change the case of properties from kebab to camel
+
+const heading= {
+  color: 'yellow',
+  textTransform: 'capitalize',
+  textAlign:"center", 
+  textShadow:'0px 2px 4px #fb4476',
+  fontWeight: 'bold',
+  margin: '70px 0px' ,
+  fontFamily: '"Chela One", cursive',
+  }
+
 ReactDom.render(
 <>
-<h1 className="heading ">hello my name is suman</h1>
+<h1 /*style={ {
+  color: 'yellow',
+  textTransform: 'capitalize', textAlign:'center'}}*/  
+    
+    // you can also do this 
+    style={heading}>{" "}hello my name is suman</h1>
 <div className="img_div">
 <img src={img1} alt="random img "></img>
 <img src={img2} alt="random img "></img>
